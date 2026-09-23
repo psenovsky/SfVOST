622c17ac92fd6f6ed638b40eb0958a196364cf81
Pavel Šenovský <pavel.senovsky@vsb.cz>
2026-09-23 14:29:22 +0200

    další zdroje

--- src/scraper/html_parser.py
+++ src/scraper/html_parser.py
@@ -218,6 +218,11 @@ def extract_body_text(html_content):
         body_text = body.get_text(strip=True)
         return body_text
 
+    if site_name == "BUSINESS DAILY NEWS : BYZNYS NOVINY":
+        body = soup.select_one('div.main-post')
+        body_text = body.get_text(strip=True)
+        return body_text
+
     # news-detail__content textpage
     t = soup.find("meta", property="og:url")
     if t:
